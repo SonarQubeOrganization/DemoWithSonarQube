903de4aefd2e661180d0da595edd2dba8b653430
PedroDavid571 <pedro_571_david@hotmail.com>
2024-05-31 08:44:51 -0400

    Updated Controller (#1)

--- DemoWithSonarQube/Controllers/WeatherForecastController.cs
+++ DemoWithSonarQube/Controllers/WeatherForecastController.cs
@@ -1,33 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace DemoWithSonarQube.Controllers
+namespace DemoWithSonarQube.Controllers;
+
+[Route("api")]
+public class WeatherForecastController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    private static readonly string[] Summaries = new[]
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Tempered", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Tempered", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    [HttpGet("weather")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
-        }
+    }
+
+    [HttpGet("cars")]
+    public async Task<IActionResult> GetCarsAsync()
+    {
+        var cars = await Task.FromResult(new List<string> { "T-Cross", "Beetle" });
+
+        return Ok(cars);
     }
 }
